a5508d159df88cd6c64c412179951c94fc85f8db
ZenithSal <salwanmax@gmail.com>
2023-09-26 16:08:52 +1300

    Stretch unpickled map to double-width all tiles except borders and center

--- scripts/unpickle_maps.py
+++ scripts/unpickle_maps.py
@@ -4,6 +4,8 @@ luafile = []
 
 luafile.append("RES_MAPS = {")
 
+skip = [1, 2, 13, 14, 25, 26]
+
 for i in range(1, 36):
     d = []
     mf = open("../res/maps/" + str(i) + ".map", "rb")
@@ -11,10 +13,18 @@ for i in range(1, 36):
     mf.close()
     l = "{\n"
     c = 1
+    lc = 0
     for v in d:
         l += str(v) + ", "
+        lc+=1
+        if not c in skip:
+            l += str(v) + ", "
+            lc+=1
         c += 1
         if c > 26:
+            #if i == 1:
+                #print("LINE LEN = " + str(lc))
+            lc = 0
             l += "\n"
             c -= 26
     l = l[:-3]
